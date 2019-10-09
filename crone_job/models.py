9f3899a98386c40a7c924718d40035bda8c44d6b
Michal Durik <michal.durik@evosys.ch>
2019-10-09 10:51:30 +0200

    Added passlib for hashed password and hasshed password before save into DB.

--- crone_job/models.py
+++ crone_job/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class CronJob(models.Model):
 	title = models.CharField(max_length=200, null=False, default='')
 	url = models.URLField(max_length=250, null=False, default='')
-	http = models.BooleanField(default=False)
+	http = models.BooleanField(default=False, null=False)
 	username = models.CharField(max_length=100, null=True, default='')
 	password = models.CharField(max_length=255, null=True, default='')
 
--- crone_job/views.py
+++ crone_job/views.py
@@ -3,79 +3,83 @@ from .models import CronJob
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from passlib.hash import pbkdf2_sha256
 
 
+# Landing page for login
 def home(request):
-    return render(request, 'home.html', {})
+	return render(request, 'home.html', {})
 
 
+# Login existing user
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'You have been logged in.')
-            return redirect('crone')
-        else:
-            messages.error(request, 'Incorrect info.')
-            return redirect('home')
-    else:
-        return render(request, 'login_user.html', {})
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'You have been logged in.')
+			return redirect('crone')
+		else:
+			messages.error(request, 'Incorrect info.')
+			return redirect('home')
+	else:
+		return render(request, 'login_user.html', {})
 
 
+# Simple logout
 def logout_user(request):
-    logout(request)
-    messages.success(request, 'You have been logout')
-    return redirect('home')
+	logout(request)
+	messages.success(request, 'You have been logout')
+	return redirect('home')
 
 
+# Create new user
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, 'You have been registered...')
-            return redirect('crone')
-    else:
-        form = SignUpForm()
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, 'You have been registered...')
+			return redirect('crone')
+	else:
+		form = SignUpForm()
 
-    context = {'form': form}
-    return render(request, 'register_user.html', context)
+	context = {'form': form}
+	return render(request, 'register_user.html', context)
 
 
+# Crone Job logic
 def crone(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        url = request.POST.get('url')
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		url = request.POST.get('url')
 
-        http = request.POST.get('http')
-        if http == 'on':
-            http = True
-        if http is None:
-            http = False
+		http = request.POST.get('http')
+		if http == 'on':
+			http = True
+		if http is None:
+			http = False
 
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        all = request.POST.get('exampleRadios')
-        allMinutes = request.POST.get("minutesAll")
+		username = request.POST.get('username')
 
-        print('GANDALF = ', http, all, allMinutes)
+		password = request.POST.get('password')
+		enc_password = pbkdf2_sha256.encrypt(password, rounds=12000, salt_size=32)
 
-        '''if title:
+		all = request.POST.get('exampleRadios')
+		allMinutes = request.POST.get("minutesAll")
+
+		print('GANDALF = ', http, enc_password, all, allMinutes)
+
+		'''if title:
             obj = CronJob.objects.create(title=title, url=url, http=http, username=username, password=password)
             obj.save()
             messages.success(request, 'You have been created new Crone...')'''
-        return render(request, 'crone.html')
-    else:
-        return render(request, 'crone.html')
-
-
-
-
-
+		return render(request, 'crone.html')
+	else:
+		return render(request, 'crone.html')
