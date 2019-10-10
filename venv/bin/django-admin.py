7cd25119302f92f9bb78828c28ee15941c78d9aa
Michal Durik <michal.durik@evosys.ch>
2019-10-10 09:27:28 +0200

    Remove venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michalevo/Desktop/My_Code/Django-New/django_crone_job/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
