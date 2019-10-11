57dc9cf3017c81100e4464b842426ccf60432a0a
Michal Durik <michal.durik@evosys.ch>
2019-10-11 09:43:51 +0200

    Fixed Settings for Heroku.

--- django_crone_job/settings.py
+++ django_crone_job/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from django.contrib.messages import constants as messages
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 LOGIN_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
